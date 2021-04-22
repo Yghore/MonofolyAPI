42ad21af3dd40fcb08f3ac2a079930da1f37d6de
Yghore <yhgore@gmail.com>
2021-04-22 21:53:07 +0200

    Api changement (Format card)
    Les carte sont retourner tout de suite au même format que dans le jeu (C'était plus simple de changer le format sur le serveur que dans le jeu)
    Pour la v1 ca suffirat amplement mais dans l'avenir trouver un moyen peut être plus efficace de passer les cartes

--- app/Http/Controllers/Api/CardController.php
+++ app/Http/Controllers/Api/CardController.php
@@ -16,13 +16,55 @@ class CardController extends Controller
      */
     public function index(string $mod, int $id = -1)
     {
+        $allCards = [];
         if($id == -1)
         {
-            $cards = DB::table('cards')->where('game', '=', $mod)->get();
-            return $cards; 
+
+            $cards = DB::table('cards')->where('game', '=', $mod)->get(['case as Case', 'n_case as Id', 'n_buttons as button_number', 'title', 'desc_case as desc', 'buttons'])->toArray();
+            foreach ($cards as $card) {
+                $buttons = explode(',', $card->buttons);
+                switch ($card->button_number) {
+                    case 1:
+                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],];
+                        break;
+                    case 2:
+                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]]];
+                        break;
+                    case 3:
+                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]],'Button_3' => [$buttons[2]]];
+                        break;                   
+                    default:
+                    $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],];
+                        break;
+                }
+
+                array_push($allCards, $formattedcards);
+                
+                
+            }
+            return $allCards;
         }
-        $cards = DB::table('cards')->where([['game', '=', $mod], ['case', '=', $id]])->get();
-        return $cards;
+        $cards = DB::table('cards')->where([['game', '=', $mod], ['case', '=', $id]])->get(['case as Case', 'n_case as Id', 'n_buttons as button_number', 'title', 'desc_case as desc', 'buttons'])->toArray();
+        foreach ($cards as $card) {
+            $buttons = explode(',', $card->buttons);
+            switch ($card->button_number) {
+                case 1:
+                    $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],];
+                    break;
+                case 2:
+                    $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]]];
+                    break;
+                case 3:
+                    $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]],'Button_3' => [$buttons[2]]];
+                    break;                   
+                default:
+                $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],];
+                    break;
+            }
+            
+            array_push($allCards, $formattedcards);
+        }
+        return $allCards;
     }
 
     /**
