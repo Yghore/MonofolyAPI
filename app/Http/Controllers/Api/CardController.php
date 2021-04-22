0901ec54f6a548028a721d18b7a72490cb722114
Yghore <yhgore@gmail.com>
2021-04-22 23:56:31 +0200

    Add quantity return api
    Oublie de la quantity pour le return api

--- app/Http/Controllers/Api/CardController.php
+++ app/Http/Controllers/Api/CardController.php
@@ -20,21 +20,21 @@ class CardController extends Controller
         if($id == -1)
         {
 
-            $cards = DB::table('cards')->where('game', '=', $mod)->get(['case as Case', 'n_case as Id', 'n_buttons as button_number', 'title', 'desc_case as desc', 'buttons'])->toArray();
+            $cards = DB::table('cards')->where('game', '=', $mod)->get(['case as Case', 'n_case as Id', 'n_buttons as button_number', 'title', 'desc_case as desc', 'buttons', 'quantity'])->toArray();
             foreach ($cards as $card) {
                 $buttons = explode(',', $card->buttons);
                 switch ($card->button_number) {
                     case 1:
-                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],];
+                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'quantity' => [$card->quantity],'Button_1' => [$buttons[0]],];
                         break;
                     case 2:
-                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]]];
+                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'quantity' => [$card->quantity],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]]];
                         break;
                     case 3:
-                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]],'Button_3' => [$buttons[2]]];
+                        $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'quantity' => [$card->quantity],'Button_1' => [$buttons[0]],'Button_2' => [$buttons[1]],'Button_3' => [$buttons[2]]];
                         break;                   
                     default:
-                    $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'Button_1' => [$buttons[0]],];
+                    $formattedcards = ['Case' => $card->Case,'Id' => [$card->Id],'Button_number' => [$card->button_number],'title' => [$card->title],'desc' => [$card->desc],'quantity' => [$card->quantity],'Button_1' => [$buttons[0]],];
                         break;
                 }
 
